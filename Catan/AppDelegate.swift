724bf4034a5cf19d29b43b6c07b85404c8c3de08
sdds7667 <inplmd@gmail.com>
2023-06-06 21:17:22 +0200

    Initial Commit

---
+++ Catan/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Catan
+//
+//  Created by Ion Plamadeala on 06/06/2023.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
